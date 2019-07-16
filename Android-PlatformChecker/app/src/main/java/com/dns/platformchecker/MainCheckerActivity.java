128fffc0f4ec8a53c2e32c4bc5aeef6a085345a0
dns <dinezh.shetty@gmail.com>
2019-07-15 21:15:52 -0400

    public push

---
+++ Android-PlatformChecker/app/src/main/java/com/dns/platformchecker/MainCheckerActivity.java
@@ -0,0 +1,82 @@
+package com.dns.platformchecker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.muddzdev.styleabletoast.StyleableToast;
+
+import life.sabujak.roundedbutton.RoundedButton;
+
+public class MainCheckerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RoundedButton emulationCheckButton1 = (RoundedButton) findViewById(R.id.checkEmulationStatus1);
+        emulationCheckButton1.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String isEmulator = checkIfDeviceIsEmulator1();
+                System.out.println("isEmulator returns = "+isEmulator);
+
+                if(isEmulator.trim().equals("yes"))
+                {
+                    StyleableToast.makeText(getApplicationContext(), "You failed!! Emulator Detected!", Toast.LENGTH_LONG, R.style.redColor2).show();
+                }
+                else if(isEmulator.trim().equals("no")) {
+                    StyleableToast.makeText(getApplicationContext(), "Congratulations!! No Emulator Detected!", Toast.LENGTH_LONG, R.style.greenColor2).show();
+                } else{
+                    StyleableToast.makeText(getApplicationContext(), "Something went wrong!!", Toast.LENGTH_LONG, R.style.redColor2).show();
+                }
+            }
+        });
+
+        RoundedButton emulationCheckButton2 = (RoundedButton) findViewById(R.id.checkEmulationStatus2);
+        emulationCheckButton2.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+
+            }
+        });
+
+        RoundedButton rootCheckButton = (RoundedButton) findViewById(R.id.checkRootStatus);
+        rootCheckButton.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+    }
+
+    public String checkIfDeviceIsEmulator1() {
+        //Normal function String returnType & No arguments
+
+        if(Build.FINGERPRINT.startsWith("generic")
+                || Build.FINGERPRINT.startsWith("unknown")
+                || Build.MODEL.contains("google_sdk")
+                || Build.MODEL.contains("Emulator")
+                || Build.MODEL.contains("Android SDK built for x86")
+                || Build.MANUFACTURER.contains("Genymotion")
+                || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+                || "google_sdk".equals(Build.PRODUCT))
+        {
+            return "yes";
+        }
+            return "no";
+
+    }
+
+}
